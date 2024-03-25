3e78ab42776b26529ed656c7fc1aa7371d750e43
robertoDCJ <robertox70@hotmail.com>
2024-03-25 19:26:07 -0300

    update

--- src/views/LivrosCadastro/LivrosCadastro.jsx
+++ src/views/LivrosCadastro/LivrosCadastro.jsx
@@ -3,7 +3,7 @@ import Header from '../../components/Header/Header'
 import "./index.scss"
 import SubmenuLivros from '../../components/SubmenuLivros/SubmenuLivros'
 import { LivrosService } from '../../api/LivrosService'
-import { Link } from 'react-router-dom'
+import { Link, useNavigate } from 'react-router-dom'
 
 const LivrosCadastro = () => {
 
@@ -25,7 +25,7 @@ const LivrosCadastro = () => {
         .then((response) => {
           alert(response.data)
           document.getElementById('formulario').reset
-          LivrosService.getLivros()
+          useNavigate('/livros')
         })
         .catch(({ response: { data, status } }) => {
           alert(`${status} - ${data}`)
