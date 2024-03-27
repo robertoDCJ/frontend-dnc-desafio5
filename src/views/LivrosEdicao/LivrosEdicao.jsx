e19765ed2c10992e27313e85c1e8b75bfb038374
robertoDCJ <robertox70@hotmail.com>
2024-03-27 17:04:24 -0300

    update

--- src/views/LivrosEdicao/LivrosEdicao.jsx
+++ src/views/LivrosEdicao/LivrosEdicao.jsx
@@ -31,6 +31,7 @@ const LivrosEdicao = () => {
     if(livro.id!=undefined && livro.id!='' && livro.titulo!=undefined && livro.titulo!='' && livro.num_paginas!=undefined && livro.num_paginas!='' && livro.isbn !=undefined && livro.isbn !='' && livro.editora !=undefined && livro.editora !=''){
       await LivrosService.updateLivro(Number(livro.id),body)
       .then(({data})=>{
+        console.log(data);
         alert(data.mensagem)
       })
       .catch(({response:{data,status}})=>{
